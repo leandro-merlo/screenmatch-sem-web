f5b92e41134f50281ca02b7fcdcc42cdf60c0fbc
Leandro Manzano Merlo <leandro.manzanomerlo@decathlon.com>
2024-08-29 14:59:51 -0300

    Reorganização da classe ConversorDados

--- src/main/java/br/com/manzatech/screenmatch/services/RecordDeserializer.java
+++ src/main/java/br/com/manzatech/screenmatch/services/RecordDeserializer.java
@@ -12,7 +12,6 @@ import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
-import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
@@ -24,6 +23,7 @@ public class RecordDeserializer<T> extends JsonDeserializer<T> {
     private final Class<T> clazz;
     private Constructor<?> constructor;
     private ArrayList<Field> fields = new ArrayList<>();
+
     public RecordDeserializer(Class<T> clazz) {
         this.clazz = clazz;
         if (!clazz.isRecord()) {
@@ -36,40 +36,47 @@ public class RecordDeserializer<T> extends JsonDeserializer<T> {
 
     @SuppressWarnings("unchecked")
     @Override
-    public T deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JacksonException {
+    public T deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
         ObjectNode node = jp.getCodec().readTree(jp);
 
         Object[] args = new Object[this.constructor.getParameterCount()];
         AtomicInteger count = new AtomicInteger(-1);
-        Arrays.stream(this.constructor.getParameters()).forEach(parameter -> {
-            int idx = count.incrementAndGet();
-            ArrayList<String> aliases = new ArrayList<>();
-            aliases.add(parameter.getName());
-            this.fields.forEach(f -> {
-                if (f.getName().equals(parameter.getName())) {
-                    if (f.isAnnotationPresent(JsonAlias.class)) {
-                        JsonAlias ja = f.getAnnotation(JsonAlias.class);
-                        aliases.addAll(Arrays.asList(ja.value()));
-                    }
-                }
-            });
-            String toSearch = parameter.getName();
-            for (String alias : aliases) {
-                if (node.has(alias)) {
-                    toSearch = alias;
-                    break;
-                }
-            }
-            Object val = node.has(toSearch) ? getNodeAs(node, toSearch, parameter) : null;
-            args[idx] = val != null ? val : dynamicValues.get(parameter.getName());
-        });
-        
+        Arrays.stream(this.constructor.getParameters())
+                .forEach(parameter -> getConstructorArguments(node, count, parameter, args));
+
         try {
             return (T) this.constructor.newInstance(args);
         } catch (InstantiationException | IllegalAccessException | IllegalArgumentException
                 | InvocationTargetException e) {
             e.printStackTrace();
             return null;
+        }
+    }
+    
+    public void addDynamicValue(String key, Object value) {
+        this.dynamicValues.put(key, value);
+    }
+
+    private void getConstructorArguments(ObjectNode node, AtomicInteger count, Parameter parameter, Object[] args) {
+        int idx = count.incrementAndGet();
+        ArrayList<String> aliases = new ArrayList<>();
+        aliases.add(parameter.getName());
+        this.fields.forEach(f -> getAliases(parameter, aliases, f));
+        String toSearch = parameter.getName();
+        for (String alias : aliases) {
+            if (node.has(alias)) {
+                toSearch = alias;
+                break;
+            }
+        }
+        Object val = node.has(toSearch) ? getNodeAs(node, toSearch, parameter) : null;
+        args[idx] = val != null ? val : dynamicValues.get(parameter.getName());
+    }
+
+    private void getAliases(Parameter parameter, ArrayList<String> aliases, Field f) {
+        if (f.getName().equals(parameter.getName()) && f.isAnnotationPresent(JsonAlias.class)) {
+            JsonAlias ja = f.getAnnotation(JsonAlias.class);
+            aliases.addAll(Arrays.asList(ja.value()));
         }
     }
 
@@ -81,11 +88,9 @@ public class RecordDeserializer<T> extends JsonDeserializer<T> {
                 return node.get(searchString).asInt();
             case "Double":
                 return node.get(searchString).asDouble();
+            default:
+                return null;
         }
-        return null;
     }
 
-    public void addDynamicValue(String key, Object value) {
-        this.dynamicValues.put(key, value);
-    }
 }
